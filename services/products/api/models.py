b2de50c31ef056bfbae687b44e8a4d3f4ef8ecd3
Dobromir Kovachev <dobromir.mail@gmail.com>
2024-02-18 11:45:02 +0200

    Product models.py modifications and migrations

--- services/products/api/models.py
+++ services/products/api/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+import uuid
 
 
 class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=200)
+    price = models.IntegerField()
     image = models.CharField(max_length=200)
     likes = models.PositiveIntegerField(default=0)
 
